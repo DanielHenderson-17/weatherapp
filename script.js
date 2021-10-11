89c3ac6706f5bd671963f6a7a1eff8c117abe802
Daniel <69319302+DanielHenderson-17@users.noreply.github.com>
2021-10-10 22:14:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,97 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY = 'a84cb3c2d1005edd54ad1eca273f6e6c';
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour;
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM';
+
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat: hoursIn12HrFormat) + ':' + (minutes < 10? '0' +minutes: minutes) + '' + `<span id="am-pm">${ampm}</span>`;
+
+    dateEl.innerHTML = days[day] + ', ' + months[month]+ ' ' + date;
+
+}, 1000); 
+
+
+getWeatherData();
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log('success');
+
+        let {latitude, longitude } = success.coords; 
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        console.log(data);
+         showWeatherData(data);
+        })
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, temp, sunrise, sunset, wind_speed} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + ' &#176;N, ' + data.lon+ ' &#176;W'
+    currentWeatherItemsEl.innerHTML = 
+    `<div class="weather-item">
+        <div>Temperature :</div>
+        <div>${Math.round(temp)}&#176; F</div>
+    </div>
+    <div class="weather-item">
+        <div>Humidity :</div>
+        <div>${humidity}%</div>
+    </div>
+    <div class="weather-item">
+        <div>Wind Speed :</div>
+        <div>${Math.round(wind_speed)} mph</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunrise :</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunset :</div>
+        <div>${window.moment(sunset * 1000).format('HH:mm a')}</div>
+    </div>`;
+
+    let otherDayForecast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx == 0){
+            currentTempEl.innerHTML = `
+            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <div class="temp">Night - ${Math.round(day.temp.night)}&#176; F</div>
+                <div class="temp">Day - ${Math.round(day.temp.day)}&#176; F</div>
+            </div>
+            `
+        }else{
+            otherDayForecast += `
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${Math.round(day.temp.night)}&#176; F</div>
+                <div class="temp">Day - ${Math.round(day.temp.day)}&#176; F</div>
+            </div>
+            `
+        }
+    })
+
+    weatherForecastEl.innerHTML = otherDayForecast;
+}
